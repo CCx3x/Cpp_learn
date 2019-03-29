0d07c4da2b29685bc63cf974e6ce8091d0629458
CCx3x <acici12138@gmail.com>
2019-03-29 11:32:06 +0800

    test2

---
+++ src/include/test.h
@@ -0,0 +1,33 @@
+// Created by cc on 2019-03-22.
+/********************************************************************************
+ *  File Name       ：   test.h
+ *  Founder         ：   cc
+ *  Creation Date   ：   2019-03-22
+ *  Description     ：   
+ *  Modifier        ：   
+ *  Modified Date   ：   
+ *  Version         ：   1.0.0
+ *  CMakeLists(cpp) ：   add_executable(test.h, "./test.h")
+ ********************************************************************************/
+#ifndef CPP_LEARN_TEST_H
+#define CPP_LEARN_TEST_H
+
+#include <iostream>
+
+using namespace std;
+
+class test
+{
+public:
+    test();
+    ~test();
+
+protected:
+
+private:
+    long card;
+    string name;
+
+};
+
+#endif //CPP_LEARN_TEST_H
