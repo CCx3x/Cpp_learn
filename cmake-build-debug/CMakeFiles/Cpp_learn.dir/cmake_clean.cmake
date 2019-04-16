9263e4ff260a999efd694f2041e998f34070ad4f
CCx3x <acici12138@gmail.com>
2019-04-16 10:43:39 +0800

    all

---
+++ cmake-build-debug/CMakeFiles/Cpp_learn.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Cpp_learn.dir/src/main/LandOwner.cpp.o"
+  "CMakeFiles/Cpp_learn.dir/src/main/LandOwnerV2.cpp.o"
+  "CMakeFiles/Cpp_learn.dir/src/main/Student.cpp.o"
+  "CMakeFiles/Cpp_learn.dir/src/main/main.cpp.o"
+  "Cpp_learn.pdb"
+  "Cpp_learn"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Cpp_learn.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- src/main/main.cpp
+++ src/main/main.cpp
@@ -19,36 +19,36 @@ using namespace std;
 
 int main()
 {
-    //this指针，使用student.h文件为例
-    Student * ptr_stu1 = new Student("大锤", "nmsl");
-    ptr_stu1->AddScore(78.99);
-    ptr_stu1->AddScore(88.99);
-    ptr_stu1->AddScore(98.99);
-    ptr_stu1->AddScore(108.99);
-    ptr_stu1->AddScore(108.99);
-    ptr_stu1->ShowInfo();
+//    //this指针，使用student.h文件为例
+//    Student * ptr_stu1 = new Student("大锤", "nmsl");
+//    ptr_stu1->AddScore(78.99);
+//    ptr_stu1->AddScore(88.99);
+//    ptr_stu1->AddScore(98.99);
+//    ptr_stu1->AddScore(108.99);
+//    ptr_stu1->AddScore(108.99);
+//    ptr_stu1->ShowInfo();
+//
+//    Student stu2 = {"尼玛", "wang"};
+//    stu2.AddScore(66.6);
+//    stu2.AddScore(76.6);
+//    stu2.AddScore(86.6);
+//    stu2.AddScore(96.6);
+//    stu2.AddScore(56.6);
+//    stu2.ShowInfo();
+//
+//    //两种书写方式一样
+//    Student& scholar1 = stu2.GetSuperScholar(*ptr_stu1);
+//    Student& scholar2 = ptr_stu1->GetSuperScholar(stu2);
+//    cout << "学霸是：" << scholar1.GetName() << endl;
+//    cout << "学霸是：" << scholar2.GetName() << endl;
+//    //scholar1在栈内存中定义的，所以main结束后，会自动释放scholar1
+//    //scholar2在栈内存中定义的，所以main结束后，会自动释放scholar2
+//    //会导致释放两次
+//    //所以在Student后加&符号，返回值是引用，所以前面也要是引用，否则会很危险！！！
+//
+//    delete(ptr_stu1);
 
-    Student stu2 = {"尼玛", "wang"};
-    stu2.AddScore(66.6);
-    stu2.AddScore(76.6);
-    stu2.AddScore(86.6);
-    stu2.AddScore(96.6);
-    stu2.AddScore(56.6);
-    stu2.ShowInfo();
 
-    //两种书写方式一样
-    Student& scholar1 = stu2.GetSuperScholar(*ptr_stu1);
-    Student& scholar2 = ptr_stu1->GetSuperScholar(stu2);
-    cout << "学霸是：" << scholar1.GetName() << endl;
-    cout << "学霸是：" << scholar2.GetName() << endl;
-    //scholar1在栈内存中定义的，所以main结束后，会自动释放scholar1
-    //scholar2在栈内存中定义的，所以main结束后，会自动释放scholar2
-    //会导致释放两次
-    //所以在Student后加&符号，返回值是引用，所以前面也要是引用，否则会很危险！！！
-
-    delete(ptr_stu1);
-
-    /*
     LandOwner* ptr_landOwner1 = new LandOwner();
     LandOwner* ptr_landOwner2 = new LandOwner("专业斗地主");
     LandOwner* ptr_landOwner3 = new LandOwner("专业地主");
@@ -62,7 +62,7 @@ int main()
     delete ptr_landOwner1;
     delete ptr_landOwner2;
     delete ptr_landOwner3;
-    */
+
 
     /*
     //建议多使用堆内存，少使用栈内存！！！
